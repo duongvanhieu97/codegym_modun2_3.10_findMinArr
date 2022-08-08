0d66b340b1053d43a51fbd0babbf89fef0de058a
Duonghieu97st <Duonghieu97st@gmail.com>
2022-08-08 10:30:41 +0700

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = newArr();
+        System.out.print("Phần tử nhỏ nhất trong mảng: " + findMinArr(arr));
+    }
+
+    public static int[] newArr() {
+        Scanner scanner = new Scanner(System.in);
+        int length;
+        System.out.println("Nhập độ dài của mảng");
+        length = scanner.nextInt();
+        int[] arr = new int[length];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Nhập phần tử thứ" + i);
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+
+    public static int findMinArr(int[] arr) {
+        int min;
+        min = arr[0];
+        for (int j = 0; j < arr.length; j++) {
+            if (min > arr[j]) {
+                min = arr[j];
+            }
+
+        }
+        return min;
+
+    }
 }
